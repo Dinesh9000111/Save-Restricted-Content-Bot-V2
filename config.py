67604fa9a466c100aeaa7668df6670ff9f97a31d
Dinesh9000111 <dinc9433@gmail.com>
2025-01-27 11:41:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ BOT_TOKEN = getenv("BOT_TOKEN", "8005393579:AAES8sv0C5NMw_vDJSTHSicCfFvdlqjRCmA"
 OWNER_ID = list(map(int, getenv("OWNER_ID", "8171835867").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://jihehod332:OM69Q4epgIEcN3xk@cluster0.qzw02.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_GROUP = getenv("LOG_GROUP", " -1002447082090")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002447082090"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "3"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "50000"))
 WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
